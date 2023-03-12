82548883d8b78097638c6aa5a1cae8b882047a43
juyoung <wanke0524@gmail.com>
2023-03-12 18:02:18 +0900

    프로젝트 생성

---
+++ src/test/kotlin/com/numble/timedeal/TimeDealApplicationTests.kt
@@ -0,0 +1,12 @@
+package com.numble.timedeal
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+class TimeDealApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
